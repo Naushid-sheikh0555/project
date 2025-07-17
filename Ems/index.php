20d2a190245ee4b2703ea9773b7f48ecaf891f2c
Naushid-sheikh0555 <naushidabbas@gmail.com>
2025-07-17 23:11:40 +0530

    Update index.php

--- Ems/index.php
+++ Ems/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Eventbrite Replica Detailed</title>
+  <title>Prasangam</title>
 
   <!-- Bootstrap CSS -->
   <link
